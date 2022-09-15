7e9d94b0a0d8c559eb3d0b3a18302e4b8cbeafe8
AdeelSarim <adeelsarim.sd@gmail.com>
2022-09-15 16:58:26 +0500

    minor fixup

--- dpk.js
+++ dpk.js
@@ -2,14 +2,11 @@ const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (key) => {
   const TRIVIAL_PARTITION_KEY = "0";
-  let customId;
 
   if (key) {
     const eventData = JSON.stringify(key);
-    customId = crypto.createHash("sha3-512").update(eventData).digest("hex");
+    return crypto.createHash("sha3-512").update(eventData).digest("hex");
   } else {
-    customId = TRIVIAL_PARTITION_KEY;
+    return TRIVIAL_PARTITION_KEY;
   }
-
-  return customId;
 };
